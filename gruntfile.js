31da61d997e0b65ac7df37aff2d04cab9411f958
GLOBAL\I019667 <takahiro.suyama@sap.com>
2019-01-10 16:42:44 +0900

    Add license report functions

---
+++ gruntfile.js
@@ -0,0 +1,14 @@
+module.exports = function( grunt ){
+	grunt.initConfig({
+		pkg: grunt.file.readJSON( 'package.json' ),
+		"grunt-license-report": {
+			output: {
+				path: './LicenseReport/licenses',
+				format: 'html'
+			}
+		}
+	});
+
+	grunt.loadNpmTasks( 'grunt-license-report' );
+	grunt.registerTask( 'default', ['grunt-license-report'] );
+};
